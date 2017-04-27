ad4a8f49499d45a124c795330f1cda439e09bc5e
Dan Brown <roooms@gmail.com>
2017-04-27 21:01:40 +0100

    WIP: Update nomad-aws inline with consul-aws
    - makes sense for the majority of the config to closely match consul-aws
    - need to figure out how best to pass in consul security groups

--- _interface.tf
+++ _interface.tf
@@ -1,31 +1,59 @@
-variable "vpc_id" {
-	type = "string"
-}
+# Required variables
 
+## how is this used?
 variable "consul_sg_id" {
-	type = "string"
+  type = "string"
 }
 
-variable "subnets" {
-	type = "list"
-}
-
-variable "cluster_size" {
-	type = "string"
-}
+##
 
 variable "cluster_name" {
-	type = "string"
+  description = "Auto Scaling Group Cluster Name"
 }
 
-variable "ami" {
-	type = "string"
+variable "os" {
+  # case sensitive for AMI lookup
+  description = "Operating System to use ie RHEL or Ubuntu"
+}
+
+variable "os_version" {
+  description = "Operating System version to use ie 7.3 (for RHEL) or 16.04 (for Ubuntu)"
+}
+
+variable "ssh_key_name" {
+  description = "Pre-existing AWS key name you will use to access the instance(s)"
+}
+
+variable "subnet_ids" {
+  description = "Pre-existing Subnet ID(s) to use"
+}
+
+variable "vpc_id" {
+  description = "Pre-existing VPC ID to use"
+}
+
+# Optional variables
+variable "cluster_size" {
+  default     = "3"
+  description = "Number of instances to launch in the cluster"
 }
 
 variable "instance_type" {
-	type = "string"
+  default     = "m4.large"
+  description = "AWS instance type to use eg m4.large"
 }
 
+variable "nomad_version" {
+  default     = "0.5.6"
+  description = "Nomad Agent version to use ie 0.5.6"
+}
+
+variable "region" {
+  default     = "us-west-1"
+  description = "Region to deploy nomad cluster ie us-west-1"
+}
+
+# Outputs
 output "asg_id" {
-	value = "${aws_autoscaling_group.nomad_server.id}"
+  value = "${aws_autoscaling_group.nomad_server.id}"
 }
--- nomad.tf
+++ nomad.tf
@@ -1,50 +1,74 @@
+module "images-aws" {
+  source        = "git@github.com:hashicorp-modules/images-aws.git?ref=dan-refactor"
+  nomad_version = "${var.nomad_version}"
+  os            = "${var.os}"
+  os_version    = "${var.os_version}"
+}
+
 resource "aws_iam_role" "nomad_server" {
-	name = "NomadServer"
-	assume_role_policy = "${data.aws_iam_policy_document.assume_role.json}"
+  name               = "${var.cluster_name}-NomadServer"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role.json}"
 }
 
 resource "aws_iam_role_policy" "nomad_server" {
-	name = "SelfAssembly"
-	role = "${aws_iam_role.nomad_server.id}"
-	policy = "${data.aws_iam_policy_document.nomad_server.json}"
+  name   = "SelfAssembly"
+  role   = "${aws_iam_role.nomad_server.id}"
+  policy = "${data.aws_iam_policy_document.nomad_server.json}"
 }
 
 resource "aws_iam_instance_profile" "nomad_server" {
-	name = "NomadServer"
-	roles = ["${aws_iam_role.nomad_server.name}"]
+  name  = "${var.cluster_name}-NomadServer"
+  roles = ["${aws_iam_role.nomad_server.name}"]
 }
 
-resource "aws_launch_configuration" "nomad_server" {
-	image_id = "${var.ami}"
-	instance_type = "${var.instance_type}"
-	security_groups = [
-		"${aws_security_group.nomad_server.id}",
-		"${var.consul_sg_id}"
-	]
-	associate_public_ip_address = false
-	ebs_optimized = false
-	iam_instance_profile = "${aws_iam_instance_profile.nomad_server.id}"
+# data "template_file" "init" {
+#   template = "${file("${path.module}/init-cluster.tpl")}"
+#
+#   vars = {
+#     region       = "${var.region}"
+#     cluster_name = "${var.cluster_name}"
+#   }
+# }
 
-	lifecycle {
-		create_before_destroy = true
-	}
+resource "aws_launch_configuration" "nomad_server" {
+  image_id      = "${module.images-aws.nomad_image}"
+  instance_type = "${var.instance_type}"
+  user_data     = "${data.template_file.init.rendered}"
+  key_name      = "${var.ssh_key_name}"
+
+  security_groups = [
+    "${aws_security_group.nomad_server.id}",
+    "${var.consul_sg_id}",                   # this needs replacing with consul-aws output
+  ]
+
+  associate_public_ip_address = false
+  ebs_optimized               = false
+  iam_instance_profile        = "${aws_iam_instance_profile.nomad_server.id}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_autoscaling_group" "nomad_server" {
-	launch_configuration = "${aws_launch_configuration.nomad_server.id}"
-	vpc_zone_identifier = ["${var.subnets}"]
+  launch_configuration = "${aws_launch_configuration.nomad_server.id}"
+  vpc_zone_identifier  = ["${var.subnet_ids}"]
+  name                 = "${var.cluster_name} Nomad Servers"
+  max_size             = "${var.cluster_size}"
+  min_size             = "${var.cluster_size}"
+  desired_capacity     = "${var.cluster_size}"
+  default_cooldown     = 30
+  force_delete         = true
 
-	name = "${var.cluster_name} Nomad Servers"
+  tag {
+    key                 = "Name"
+    value               = "${format("%s Nomad Server", var.cluster_name)}"
+    propagate_at_launch = true
+  }
 
-	max_size = "${var.cluster_size}"
-	min_size = "${var.cluster_size}"
-	desired_capacity = "${var.cluster_size}"
-	default_cooldown = 30
-	force_delete = true
-
-	tag {
-		key = "Name"
-		value = "${format("%s Nomad Server", var.cluster_name)}"
-		propagate_at_launch = true
-	}
+  tag {
+    key                 = "Cluster-Name"
+    value               = "${var.cluster_name}"
+    propagate_at_launch = true
+  }
 }
